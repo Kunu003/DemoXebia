6c75aef790f62d18435595e485a8caf70b1f3d97
salim <salimpradhan83@yahoo.com>
2019-12-06 19:49:23 +0530

    Test added

---
+++ app/src/androidTest/java/com/example/xebianytimesdemo/mvList/MostViewedViewModelTest.java
@@ -0,0 +1,18 @@
+package com.example.xebianytimesdemo.mvList;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Salim on 12/6/2019.
+ */
+public class MostViewedViewModelTest {
+
+    MostViewedViewModel mostViewedViewModel;
+
+    @Before
+    public void setSUT(MostViewedViewModel SUT) {
+        this.mostViewedViewModel = SUT;
+    }
+}
---
+++ app/src/androidTest/java/com/example/xebianytimesdemo/test/MostViewedListActivityTest.java
@@ -0,0 +1,20 @@
+package com.example.xebianytimesdemo.test;
+
+import com.example.xebianytimesdemo.mvList.MostViewedListActivity;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Salim on 12/6/2019.
+ */
+public class MostViewedListActivityTest {
+
+    MostViewedListActivity mostViewedListActivity;
+
+    @Before
+    public void setMostViewedListActivity(MostViewedListActivity mostViewedListActivity) {
+        this.mostViewedListActivity = mostViewedListActivity;
+    }
+}
